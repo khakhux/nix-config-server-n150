0b66da170340c4443229fcae53c21bb980515c45
cacu <cacu@email.com>
2025-11-23 21:48:22 +0100

    module for common config

---
+++ modules/common-configuration.nix
@@ -0,0 +1,45 @@
+# modules/common-configuration.nix
+{ config, pkgs, ... }:
+
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  time.timeZone = "Europe/Madrid";
+
+  i18n.defaultLocale = "es_ES.UTF-8";
+  console = {
+    #   font = "Lat2-Terminus16";
+    keyMap = "es";
+    #   useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  users.users.cacu = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable 'sudo' for the user.
+    openssh.authorizedKeys.keyFiles = [
+      ../ssh-keys/id_ed25519_nixos.pub  # Note: relative path adjusted
+    ];
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  programs.nix-ld.enable = true; # for remote access via vscode
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    git
+    htop
+    tmux
+  ];
+
+  networking.firewall.enable = false;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
---
+++ modules/desktop-configuration.nix
@@ -0,0 +1,17 @@
+
+# Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # programs.firefox.enable = true;
