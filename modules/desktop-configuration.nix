2e6d5c9b02cf4804e1cf41b0d961d16c2fa33b2d
cacu <cacu@email.com>
2025-11-27 15:59:26 +0100

    more commented desktop config options

--- modules/desktop-configuration.nix
+++ modules/desktop-configuration.nix
@@ -11,6 +11,11 @@
   # services.pipewire = {
   #   enable = true;
   #   pulse.enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+    # If you want to use JACK applications, uncomment this
+  #   jack.enable = true;
+
   # };
 
   # Enable the X11 windowing system.
@@ -26,6 +31,8 @@
   # Configure keymap in X11
   # services.xserver.xkb.layout = "es";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # console.keyMap = "es"
 
   # Enable RDP Access
   # services.xrdp.enable = true;
