2cd6b1c344369b3c1c46bf9c608977e7aca53604
sgen0291 <crparedes@igae.hacienda.gob.es>
2025-12-08 21:36:30 +0100

    incluyo config mininas

---
+++ hosts/mininas/configuration.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+
+let
+  ports = import ../../ports.nix;
+  ips = import ../../ips.nix;
+in
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      (import ../../modules/common-configuration.nix {
+        inherit config lib pkgs;
+        interfaceName = "ens18";
+        ipAddress = ips.mininas;
+        extraGroups = "docker";
+      })
+      ../../modules/docker.nix
+    ];
+
+  networking = {
+    hostName = "mininas";
+    networkmanager.enable = true;
+    firewall.enable = true;
+    firewall.allowedTCPPorts = [ ports.SSH ports.FRIGATE ];
+  };
+
+  ssh.allowedUsers = [ "cacu" ];
+
+  # Enable automatic updates (optional but good for servers)
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = false;
+
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
