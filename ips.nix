92119994047da9ca116ba76ce4f679d80818150a
cacu <cacu@email.com>
2025-12-09 14:53:05 +0100

    ips are strings

--- ips.nix
+++ ips.nix
@@ -1,8 +1,8 @@
 {
-  raspberry2 = 192.168.1.241;
-  raspberry4 = 192.168.1.242;
-  raspberry42 = 192.168.1.243;
-  proxmox = 192.168.1.244;
-  server-docker-01 = 192.168.1.248;
-  mininas = 192.168.1.249;
+  raspberry2 = "192.168.1.241";
+  raspberry4 = "192.168.1.242";
+  raspberry42 = "192.168.1.243";
+  proxmox = "192.168.1.244";
+  server-docker-01 = "192.168.1.248";
+  mininas = "192.168.1.249";
 }
