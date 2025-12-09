1c35ca0e55068786183cd04c8a62854b6ecc54cf
sgen0291 <crparedes@igae.hacienda.gob.es>
2025-12-09 14:41:12 +0100

    added proxmox and mininas ips

--- ips.nix
+++ ips.nix
@@ -1,5 +1,8 @@
 {
+  raspberry2 = 192.168.1.241;
+  raspberry4 = 192.168.1.242;
   raspberry42 = 192.168.1.243;
-  mininas = 192.168.1.244;
+  proxmox = 192.168.1.244;
   server-docker-01 = 192.168.1.248;
+  mininas = 192.168.1.249;
 }
